b3efcde8b168d66e914cf87e11fdf076e0a3d42b
alefenba <alefe.182@outlook.com>
2022-02-24 16:19:22 -0300

    Criado o modelo de Turma

--- pypro/turmas/models.py
+++ pypro/turmas/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 
 class Turma(models.Model):
     nome = models.CharField(max_length=64)
     slug = models.SlugField(max_length=64)
     inicio = models.DateField()
     fim = models.DateField()
+    matriculas = models.ManyToManyField(get_user_model())
