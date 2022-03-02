2e0ec983aa79afb1c81b5b2deae5eb9864a58866
alefenba <alefe.182@outlook.com>
2022-03-01 21:34:50 -0300

    Adcionado o botão de login

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,4 +1,6 @@
+from pypro.django_assertions import assert_contains
 from django.urls import reverse
+from model_bakery import baker
 import pytest
 
 
@@ -9,3 +11,32 @@ def resp(client, db):
 
 def test_login_form_page(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def usuario(db, django_user_model):
+    usuario_modelo = baker.make(django_user_model)
+    senha = 'senha'
+    usuario_modelo.set_password(senha)
+    usuario_modelo.save()
+    usuario_modelo.senha_plana = senha
+    return usuario_modelo
+
+
+@pytest.fixture
+def resp_post(client, usuario):
+    return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
+
+
+def test_logi_redirect(resp_post):
+    assert resp_post.status_code == 302
+    assert resp_post.url == reverse('modulos:indice')
+
+
+@pytest.fixture
+def resp_home(client, db):
+    return client.get(reverse('base:home'))
+
+
+def test_botao_entrar_disponivel(resp_home):
+    assert_contains(resp_home, 'Entrar')
