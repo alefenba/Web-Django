6f69ca27a67989ae15555e23b2d96579f048f34e
alefenba <alefe.182@outlook.com>
2022-03-01 23:22:28 -0300

    Adcionado botão Sair

--- pypro/settings.py
+++ pypro/settings.py
@@ -32,6 +32,7 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 AUTH_USER_MODEL = 'base.User'
 LOGIN_REDIRECT_URL = '/modulos/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # Application definition
