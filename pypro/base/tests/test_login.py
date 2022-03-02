8cb0b460613aba7e35f7b29fde2c68ee6f96c344
alefenba <alefe.182@outlook.com>
2022-03-01 23:10:18 -0300

    Adcionado função 'Sair', após estar logado

--- pypro/base/tests/test_login.py
+++ pypro/base/tests/test_login.py
@@ -1,7 +1,7 @@
-from pypro.django_assertions import assert_contains
+import pytest
+from pypro.django_assertions import assert_contains, assert_not_contains
 from django.urls import reverse
 from model_bakery import baker
-import pytest
 
 
 @pytest.fixture
@@ -28,7 +28,7 @@ def resp_post(client, usuario):
     return client.post(reverse('login'), {'username': usuario.email, 'password': usuario.senha_plana})
 
 
-def test_logi_redirect(resp_post):
+def test_login_redirect(resp_post):
     assert resp_post.status_code == 302
     assert resp_post.url == reverse('modulos:indice')
 
@@ -40,3 +40,28 @@ def resp_home(client, db):
 
 def test_botao_entrar_disponivel(resp_home):
     assert_contains(resp_home, 'Entrar')
+
+
+def test_link_de_login_disponivel(resp_home):
+    assert_contains(resp_home, reverse('login'))
+
+
+@pytest.fixture
+def resp_home_com_usuario_logado(client_com_usuario_logado, db):
+    return client_com_usuario_logado.get(reverse('base:home'))
+
+
+def test_botao_entrar_indisponivel(resp_home_com_usuario_logado):
+    assert_not_contains(resp_home_com_usuario_logado, 'Entrar')
+
+
+def test_link_de_login_indisponivel(resp_home_com_usuario_logado):
+    assert_not_contains(resp_home_com_usuario_logado, reverse('login'))
+
+
+def test_botao_sair_disponivel(resp_home_com_usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, 'Sair')
+
+
+def teste_nome_usuario_logado_disponivel(resp_home_com_usuario_logado, usuario_logado):
+    assert_contains(resp_home_com_usuario_logado, usuario_logado.first_name)
